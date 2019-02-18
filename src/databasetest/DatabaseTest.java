e5623ae92034afdfd63d79eee2a1ee00bae961ae
Brannon Ogle <bogle@stu.jsu.edu>
2019-02-17 18:30:48 -0600

    Part 2 Complete

---
+++ src/databasetest/DatabaseTest.java
@@ -0,0 +1,151 @@
+package databasetest;
+
+import java.sql.*;
+import org.json.simple.*;
+
+public class DatabaseTest {
+
+    public static void main(String[] args){
+
+        DatabaseTest.getJSONData();
+
+    }
+
+    public static JSONArray getJSONData(){
+
+        Connection conn = null;
+        PreparedStatement pstSelect = null, pstUpdate = null;
+        ResultSet resultset = null;
+        ResultSetMetaData metadata = null;
+        
+        String query, key;
+        
+        boolean hasresults;
+        int resultCount, columnCount;
+        
+        JSONArray JAObjects = new JSONArray();
+        JSONArray heading = new JSONArray();
+      
+        try {
+            
+            /* Identify the Server */
+            
+            String server = ("jdbc:mysql://localhost/p2_test");
+            String username = "root";
+            String password = "CS310";
+            System.out.println("Connecting to " + server + "...");
+            
+            /* Load the MySQL JDBC Driver */
+            
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            
+            /* Open Connection */
+
+            conn = DriverManager.getConnection(server, username, password);
+
+            /* Test Connection */
+            
+            if (conn.isValid(0)) {
+                
+                /* Connection Open! */
+                
+                System.out.println("Connected Successfully!");
+                
+                /* Prepare Select Query */
+                
+                query = "SELECT * FROM people";
+                pstSelect = conn.prepareStatement(query);
+                
+                /* Execute Select Query */
+                
+                System.out.println("Submitting Query ...");
+                
+                hasresults = pstSelect.execute();                
+                
+                /* Get Results */
+                
+                System.out.println("Getting Results ...");
+                
+                while ( hasresults || pstSelect.getUpdateCount() != -1 ) {
+
+                    if ( hasresults ) {
+                        
+                        /* Get ResultSet Metadata */
+                        
+                        resultset = pstSelect.getResultSet();
+                        metadata = resultset.getMetaData();
+                        columnCount = metadata.getColumnCount();
+                        
+                        /* Get Column Names; Print as Table Header */
+                        
+                        for (int i = 2; i <= columnCount; i++) {
+
+                            key = metadata.getColumnLabel(i);
+                            heading.add(key);
+
+                        }
+                        
+                        /* Get Data; Print as Table Rows */
+                        
+                        while(resultset.next()) {
+                            
+                            /* Begin Next ResultSet Row */
+
+                            System.out.println();
+                            
+                            /* Loop Through ResultSet Columns; Print Values */
+                            JSONObject JObject = new JSONObject(); 
+                            for (int i = 0; i <= columnCount - 2; i++) {
+
+                                String column = (String) heading.get(i);
+                                JObject.put(heading.get(i), resultset.getString(column));
+                                
+                            }
+                            JAObjects.add(JObject);
+                        }
+                        
+                    }
+
+                    else {
+
+                        resultCount = pstSelect.getUpdateCount();  
+
+                        if ( resultCount == -1 ) {
+                            break;
+                        }
+
+                    }
+                    
+                    /* Check for More Data */
+
+                    hasresults = pstSelect.getMoreResults();
+
+                }
+                
+            }
+            
+            /* Close Database Connection */
+            
+            conn.close();
+            
+        }
+        
+        catch (Exception e) {
+            System.err.println(e.toString());
+        }
+        
+        /* Close Other Database Objects */
+        
+        finally {
+            
+            if (resultset != null) { try { resultset.close(); resultset = null; } catch (Exception e) {} }
+            
+            if (pstSelect != null) { try { pstSelect.close(); pstSelect = null; } catch (Exception e) {} }
+            
+            if (pstUpdate != null) { try { pstUpdate.close(); pstUpdate = null; } catch (Exception e) {} }
+            
+        }
+        return JAObjects;
+    }
+    
+}
